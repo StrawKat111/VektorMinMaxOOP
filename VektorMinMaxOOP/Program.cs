2b4ee3aa2c83a26afdb4c55cb4ad7679e24004d6
ezegy <ezegy@DESKTOP-9POM430>
2024-11-03 20:18:18 +0100

    Add project files.

---
+++ VektorMinMaxOOP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VektorMinMaxOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VektorMinMax vektorMinMax = new VektorMinMax();
+
+            int[] vektor = { 5, 1, 8, 7, 9 };
+            vektorMinMax.SetVektor(vektor);
+            vektorMinMax.SetMin();
+            vektorMinMax.SetMax();
+            
+            (int minErtek, int minIndex, int maxErtek, int maxIndex) = vektorMinMax.GetMinMax();
+            Console.WriteLine($"A legkisebb elem: {minErtek} (index: {minIndex})");
+            Console.WriteLine($"A legnagyobb elem: {maxErtek} (index: {maxIndex})");
+        }
+    }
+}
---
+++ VektorMinMaxOOP/VektorMinMax.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VektorMinMaxOOP
+{
+    internal class VektorMinMax
+    {
+        private int[] vektor;
+        private int minErtek;
+        private int minIndex;
+        private int maxErtek;
+        private int maxIndex;
+
+        
+        public VektorMinMax() { }
+
+        
+        public VektorMinMax(int[] vektor)
+        {
+            SetVektor(vektor);
+        }
+
+        
+        public void SetVektor(int[] vektor)
+        {
+            this.vektor = vektor;
+        }
+
+        
+        public void SetMin()
+        {
+            minErtek = vektor[0];
+            minIndex = 0;
+            for (int i = 1; i < vektor.Length; i++)
+            {
+                if (vektor[i] < minErtek)
+                {
+                    minErtek = vektor[i];
+                    minIndex = i;
+                }
+            }
+        }
+
+        
+        public void SetMax()
+        {
+            maxErtek = vektor[0];
+            maxIndex = 0;
+            for (int i = 1; i < vektor.Length; i++)
+            {
+                if (vektor[i] > maxErtek)
+                {
+                    maxErtek = vektor[i];
+                    maxIndex = i;
+                }
+            }
+        }
+
+        
+        public (int ertek, int index) GetMin()
+        {
+            return (minErtek, minIndex);
+        }
+
+        
+        public (int ertek, int index) GetMax()
+        {
+            return (maxErtek, maxIndex);
+        }
+
+        
+        public (int minErtek, int minIndex, int maxErtek, int maxIndex) GetMinMax()
+        {
+            return (minErtek, minIndex, maxErtek, maxIndex);
+        }
+    }
+}
